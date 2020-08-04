9c4e6b884608b67ec85293668dbda2df9dc161b6
kikuchi yusuke <yusuke.kikuchi@outlook.com>
2020-08-04 12:58:00 +0900

    fix not incluede zenject inject

--- RemoteConnectionCore/EyeTracking/Application/DataSenderService.cs
+++ RemoteConnectionCore/EyeTracking/Application/DataSenderService.cs
@@ -1,11 +1,11 @@
 ﻿using RemoteConnectionCore.EyeTracking.Domain;
-
+using Zenject;
 namespace RemoteConnectionCore.EyeTracking.Application
 {
     public class DataSenderService
     {
         private readonly IEyeDataSender eyeDataSender;
-
+        [Inject]
         public DataSenderService(IEyeDataSender eyeDataSender)
         {
             this.eyeDataSender = eyeDataSender;
--- RemoteConnectionCore/HeadTracking/Application/DataSenderService.cs
+++ RemoteConnectionCore/HeadTracking/Application/DataSenderService.cs
@@ -1,11 +1,11 @@
 ﻿using RemoteConnectionCore.HeadTracking.Domain;
-
+using Zenject;
 namespace RemoteConnectionCore.HeadTracking.Application
 {
     public class DataSenderService
     {
         private readonly IHeadDataSender dataSender;
-
+        [Inject]
         public DataSenderService(IHeadDataSender dataSender)
         {
             this.dataSender = dataSender;
